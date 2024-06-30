5e26ce54bb78869d2e9a58755541030d9840d2a0
Jan Jędrzejewski <46831775+Luvkitri@users.noreply.github.com>
2024-06-30 21:41:24 +0200

    Update get_data.sh

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+URL="https://mtgjson.com/api/v5/AllPrintings.sqlite"
+FILENAME="/app/data/data.json"
+
+if ! type curl >/dev/null 2>&1; then
+  echo "Error: curl is not installed. Please install curl before running this script."
+  exit 1
+fi
+
+curl -s "$URL" -o "$FILENAME"
+
+if [ $? -eq 0 ]; then
+  echo "Data downloaded successfully and saved to: $FILENAME"
+else
+  echo "Error: Failed to download data from $URL"
+fi
+
